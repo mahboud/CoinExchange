4d6f549f446a11a862f46168e65dd785294f05a3
Mahboud Zabetian <mahboud@optimized.com>
2017-08-04 23:28:40 -0700

    Initial Commit

---
+++ CoinExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoinExchange
+//
+//  Created by mahboud on 8/4/17.
+//  Copyright © 2017 BitsOnTheGo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoinExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoinExchange
+//
+//  Created by mahboud on 8/4/17.
+//  Copyright © 2017 BitsOnTheGo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
